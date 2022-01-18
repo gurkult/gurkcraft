959bbd727a62e84425ed862e472e81d0703e2af2
Matteo Bertucci <matteobertucci2004@gmail.com>
2022-01-18 15:57:00 +0100

    Shredder bossbar and ticket exchange

--- src/generators/_base.py
+++ src/generators/_base.py
@@ -33,7 +33,7 @@ class BaseGenerator(ABC):
         pass
 
     @staticmethod
-    def _format_command(command: str, **kwargs: str) -> str:
+    def _format_command(command: str, **kwargs: str|int|float) -> str:
         for key, value in kwargs.items():
             command = command.replace(f"${key}", str(value))
         return command
